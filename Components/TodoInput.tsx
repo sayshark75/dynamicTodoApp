0da8e6b3c0c9a9bc9efbdcb2fe22a876a4fec726
sayyedsharuk <sayyedsharuk75@gmail.com>
2023-01-21 09:51:17 +0530

    Minor Fixes/Changes Empty Input Fix

--- Components/TodoInput.tsx
+++ Components/TodoInput.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Input, Button, Flex } from "@chakra-ui/react";
+import { Input, Button, Flex, Box, useToast } from "@chakra-ui/react";
 import { useAppDispatch, useAppSelector } from "@/Hooks/app_hooks";
 import { todoObj } from "@/Redux/todo.reducer";
 import { addTodoApi } from "@/Redux/todos.actions";
@@ -7,18 +7,30 @@ import { addTodoApi } from "@/Redux/todos.actions";
 type Props = {};
 
 const TodoInput = (props: Props) => {
+  const toast = useToast();
   const { loading } = useAppSelector((store) => store.todo_m);
   const [todoTitle, setTodoTitle] = useState<string>("");
   const dispatch = useAppDispatch();
 
   const HandleSubmit: () => void = () => {
-    let obj: todoObj = {
-      id: Date.now(),
-      title: todoTitle,
-      status: false,
-    };
-    dispatch(addTodoApi(obj));
-    setTodoTitle("");
+    if (todoTitle !== "") {
+      let obj: todoObj = {
+        id: Date.now(),
+        title: todoTitle,
+        status: false,
+      };
+      dispatch(addTodoApi(obj));
+      setTodoTitle("");
+    } else {
+      toast({
+        position: "bottom-left",
+        render: () => (
+          <Box color="white" p={3} bg="green.500">
+            Please Enter Something
+          </Box>
+        ),
+      });
+    }
   };
 
   return (
